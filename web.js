b298a27a7430305d8602d8c9a689b885061cbab8
Mamas Nicolaou <mamasnicolaou@gmail.com>
2013-07-23 19:32:31 +0100

    applied fix2 adding buffer to string conversion

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   data=fs.readFileSync(indexFile);
-  response.send(data);
+  response.send(data.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
